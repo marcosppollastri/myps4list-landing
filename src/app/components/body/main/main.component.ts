1aca20eb0da6654197303d5d2c5de96910252a47
Marcos J. Pena Pollastri <marcos.p.pollastri@gmail.com>
2019-09-04 12:04:55 -0300

    se agrego los botones de los stores

--- src/app/components/body/main/main.component.ts
+++ src/app/components/body/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  faGamepad;
   constructor() { }
 
   ngOnInit() {
+    this.faGamepad = faGamepad;
   }
 
 }
